9d7fa299f5e3411562dcb28cbdea4c7d34e95458
pioj <piojete@gmail.com>
2019-04-11 05:15:13 +0200

    Unity Project & 1st Build Test

---
+++ Assets/Scripts/touchfacil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class touchfacil : MonoBehaviour
+{
+    public TMP_Text textToques;
+    public int toques;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        toques++;
+        textToques.text = "Toques: " + toques.ToString();
+    }
+}
